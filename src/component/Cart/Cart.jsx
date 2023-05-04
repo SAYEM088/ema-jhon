6caf11246496547a9cdea946ddc8ea070d56e6fb
SAYEM088 <abusayemsarker088@gmail.com>
2023-05-04 13:41:17 +0600

    show data from cart

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -6,16 +6,22 @@ const Cart = ({Cart}) => {
     console.log(Cart);
     let totalPrice = 0;
     let totalShipping =0;
+    let  quantity = 0;
     for (const product of Cart){
-        totalPrice =totalPrice + product.price;
-        totalShipping =totalShipping + product.shipping
+        if(product.quantity === 0){
+            product.quantity = 1;
+        }
+        // product.quantity = product.quantity || 1;
+        totalPrice =totalPrice + product.price * product.quantity;
+        totalShipping =totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice *7 /100;
     const grandTotal = totalPrice + totalShipping + tax ;
     return (
         <div className='cart'>
              <h2>Order Summery</h2>
-                <p>Selected Items : <b> {Cart.length}</b></p>
+                <p>Selected Items : <b> {quantity}</b></p>
                 <p>Total Price : <b> $ {totalPrice}</b></p>
                 <p>Total Shipping: <b> $ {totalShipping}</b></p>
                 <p>Tax: <b> $ {tax.toFixed(2)}</b></p>
